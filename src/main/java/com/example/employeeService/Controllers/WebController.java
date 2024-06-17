996118f68fbce067bf305a666b9c669f630f3441
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-06-17 19:20:27 +0530

    updated web controller for stati csite

--- src/main/java/com/example/employeeService/Controllers/WebController.java
+++ src/main/java/com/example/employeeService/Controllers/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
     @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
-        // Forward to index.html so that the client-side routing can take over
+        // Forward to index.html so that Angular routing can handle the route
         return "forward:/index.html";
     }
 }
